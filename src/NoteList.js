6de8ae0479b6f7e7017bb2a596accf840bdec449
Nicholas Loyd <Imatleast12@gmail.com>
2017-07-10 11:33:38 -0400

    Use destructring assignment on props

--- src/NoteList.js
+++ src/NoteList.js
@@ -2,20 +2,22 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = (props) =>{
-  const noteIDs = Object.keys(props.notes)
-    return (        
-      <div className="NoteList">
-        <h3>Notes</h3>
-          <ul id="notes">
-            {noteIDs.map(noteID=> 
-              <Note 
-                key={noteID}
-                note={props.notes[noteID]} 
-                setCurrentNote = {props.setCurrentNote}/> 
-            )}}
-          </ul>
-      </div>
-    )
+const NoteList = ({notes, setCurrentNote}) =>{
+  const noteIds = Object.keys(notes)
+    
+  return (        
+    <div className="NoteList">
+      <h3>Notes</h3>
+        <ul id="notes">
+          {noteIds.map(noteId => (
+            <Note 
+              key={noteId}
+              note={notes[noteId]} 
+              setCurrentNote = {setCurrentNote}
+            /> 
+          ))}
+        </ul>
+    </div>
+  )
 }
 export default NoteList
